90bb03bf04bfdcdb04b6a3f077bd8c73b8d7a7e7
Daniela-Martinez <d.rdz.240615@gmail.com>
2020-10-03 20:51:19 -0500

    Ejercicio de APIS

---
+++ Practica1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# -*- ENCODING: UTF-8 -*-
+#1
+Remove-item alias curl
+curl -v -H "d-apikey: bbf458bc5f6397a157c14c6fc52adcabcd5a95c9" https://hunter.io/api-keys
+
+#2
+Remove-item alias curl
+curl -v -H "d-apikey:eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJkYW5pZWxhLm1hcnRpbmV6cmRyQHVhbmwuZWR1Lm14IiwianRpIjoiYWMyMzE5MzItNDJjMC00M2FhLWI0MDgtNTI5MTYyMmNhMDNjIiwiaXNzIjoiQUVNRVQiLCJpYXQiOjE2MDE3NzE5NTgsInVzZXJJZCI6ImFjMjMxOTMyLTQyYzAtNDNhYS1iNDA4LTUyOTE2MjJjYTAzYyIsInJvbGUiOiIifQ.d5WduC1veV6wbpaEEiRmA3ttCAuV5pJxRQatKCOYp0E "https://opendata.aemet.es/centrodedescargas/obtencionAPIKey
+
+#3
+Remove-item alias curl
+curl -v -H "d-apikey: d232f3b52amshb1d1daa58a7354dp1117bejsn2ccc2d1da993" https://aerisweather1.p.rapidapi.com/observations/summary/buenos%2520aires%252Car
+
